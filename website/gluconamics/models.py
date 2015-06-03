09f2eb6db3330e7823f469d48fc3ed9cac79a854
Matthias Koenig <konigmatt@googlemail.com>
2015-06-03 10:07:19 +0200

    First static files added and run of syncdb

--- website/gluconamics/models.py
+++ website/gluconamics/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 
-class Visiter(models.model):
+class Visiter(models.Model):
     email = models.EmailField()
     date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):   
+        return self.email
