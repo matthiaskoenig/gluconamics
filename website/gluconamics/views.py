8ba85662f4b22fb8cbab83fce07fba09c1b3d466
Matthias Koenig <konigmatt@googlemail.com>
2015-06-03 14:09:01 +0200

    First release version

--- website/gluconamics/views.py
+++ website/gluconamics/views.py
@@ -9,3 +9,22 @@ def index(request):
     return HttpResponse(template.render(context))
 
     # return HttpResponse("Hello, world. You are visiting gluconamics.")
+
+def newsletter(request):
+    print request.POST
+
+    try:
+        email = request.POST['email']
+    except KeyError:
+        # Redisplay the homepage
+        return render(request, 'index.html', {
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        pass
+
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        # return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+
